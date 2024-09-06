0c7894b64637f6ab835d29de9f69cd9e1c336717
Denis <denis-velo.pro@mail.ru>
2024-09-06 14:29:35 +0500

    enable tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  // modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss'],
   server: {
     port: parseInt(process.env.PORT || '3000', 10) // Используйте переменную окружения PORT
   }
