e3ce87480baf2e7ed048375d29fe58b128d11cdf
Denis <denis-velo.pro@mail.ru>
2024-09-06 19:31:33 +0500

    Add russian language

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1 class="text-2xl font-bold text-center mt-4">Posts</h1>
-    <PostTable />
+    <h1 class="text-2xl font-bold text-center mt-8">Список постов</h1>
+    <PostTable/>
   </div>
 </template>
 
