3b554c9ff72045fca27177134dece5ba771865c3
Denis <denis-velo.pro@mail.ru>
2024-09-06 12:35:09 +0500

    add port nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  server: {
+    port: parseInt(process.env.PORT || '3000', 10) // Используйте переменную окружения PORT
+  }
 })
